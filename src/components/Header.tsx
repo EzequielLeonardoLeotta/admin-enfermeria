b2c500b67bf803139a2b372cd7bb8124dfdba3d6
Ezequiel Leonardo Leotta <leotta010@hotmail.com>
2023-04-16 01:27:46 -0300

    added sign in page

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import React from 'react'
+
+function Header() {
+    return (
+        <Head>
+            <title>Administracion de Enfermeria</title>
+            <link rel="icon" href="/unla-icon.jpg" />
+        </Head>
+    )
+}
+
+export default Header
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import SignInPage from './sign-in/index.'
+import Header from '@/components/Header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <SignInPage />
+    </>
+  )
+}
